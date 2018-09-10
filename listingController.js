d2dbfd5ef0ed580ec8da44275971367e964f16e2
Daniel Frystacky <danielfrystacky@gmail.com>
2018-09-10 14:01:17 -0400

    seach bar is up and running

--- listingController.js
+++ listingController.js
@@ -11,6 +11,32 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       },
       "address":""
     };
+    $scope.filteredListings = $scope.listings;
+
+    document.getElementById("jumbotronDisplay").style.display = "none";
+
+    // $scope.searchText was created as a model in our HTML search input bar
+
+    $scope.applyFilter = function() {
+      if ($scope.searchText.length == 0) {
+        $scope.filteredListings = $scope.listings;
+      } else {
+        var matchOrNot = false;
+        $scope.filteredListings = [];
+        for (x = 0; x < $scope.listings.length; x++) {
+          if ($scope.listings[x].name.toUpperCase().includes($scope.searchText.toUpperCase())) {
+            matchOrNot = true;
+          }
+          if ($scope.listings[x].code.toUpperCase().includes($scope.searchText.toUpperCase())) {
+            matchOrNot = true;
+          }
+          if (matchOrNot) {
+            $scope.filteredListings.push($scope.listings[x]);
+          }
+          matchOrNot = false;
+        } // end for loop
+      } // end else block
+    } // end function
 
     /*
       Implement these functions in the controller to make your application function
@@ -19,7 +45,8 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.addListing = function() {};
     $scope.deleteListing = function(index) {};
     $scope.showDetails = function(index) {
-      $scope.jumbotronlisting = $scope.listings[index];
+      document.getElementById("jumbotronDisplay").style.display = "initial";
+      $scope.jumbotronlisting = $scope.filteredListings[index];
     };
   }
 ]);
