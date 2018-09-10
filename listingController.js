6d11617442b9555b51d66c194f0327f8c5d4c725
Daniel Frystacky <danielfrystacky@gmail.com>
2018-09-10 01:27:53 -0400

    some stuff working

--- listingController.js
+++ listingController.js
@@ -1,14 +1,25 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.jumbotronlisting = {
+      "code":"",
+      "name":"",
+      "coordinates":{
+        "latitude":"",
+        "longitude":""
+      },
+      "address":""
+    };
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
     $scope.addListing = function() {};
     $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+      $scope.jumbotronlisting = $scope.listings[index];
+    };
   }
 ]);
