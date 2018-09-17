a59e9760388ef37e49c6a97c56070f6cd3fdcb1a
Daniel Frystacky <danielfrystacky@gmail.com>
2018-09-16 21:44:05 -0400

    Everything is working

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,41 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  name: {type: String, required: true},
+  code: {type: String, required: true},
+  address: String,
+  coordinates:
+    {
+      latitude: Number,
+      longitude: Number
+    },
+
+  created_at: Date,
+  updated_at: Date
+
+
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+
+  //from schotch.io at "Run a function before saving"
+
+   var currentDate = new Date();   // gets currnet date
+
+   this.updated_at = currentDate;  //updates currnet date
+
+   if(!this.created_at)
+        this.created_at = currentDate;  // adds if created date
+
+    next();
+
+
 });
 
 /* Use your schema to instantiate a Mongoose model */
